d843de26b9e044ec52050bfa61b1fda1b5193f5e
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2020-12-18 01:54:16 -0500

    Update mysql.sh

--- mysql.sh
+++ mysql.sh
@@ -18,30 +18,19 @@ mysql_secure_installation
 
 
 clear
-echo ""
-echo ""
-echo ""
+
 echo "***************************************************************************"
 echo -e "   \e[1;32mPlease complete the configuration in the database: ----  \e[0m  "
 echo "***************************************************************************"
 echo ""
 echo -n -e "  \e[1;32mEnter the database root password: ----  ";  read PASSWORD
-echo ""
 
 # To create an new user id and password in the database
 echo ""
 echo -n -e "  \e[1;32mEnter the new user id: ----   \e[0m";       read CREATE_USER_NAME
-echo ""
-echo ""
 echo -n -e "  \e[1;32mEnter the new user password: ----  \e[0m";  read USER_PASSWORD
-echo ""
-echo ""
-
-echo ""
-echo ""
 echo -n -e "  \e[1;32mPlease Enter the database name: ----  \e[0m"; read DB
-echo ""
-echo ""
+
 
 # To create a Database. 
 mysql -u root -p$PASSWORD -e "CREATE DATABASE $DB;"
@@ -50,17 +39,6 @@ mysql -u root -p$PASSWORD -e "CREATE DATABASE $DB;"
 mysql -u root -p$PASSWORD -e "CREATE USER '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
 mysql -u root -p$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO '$CREATE_USER_NAME'@'localhost' IDENTIFIED BY '$USER_PASSWORD';"
 
-echo ""
-echo ""
-echo ""
 echo "***************************************************************************"
-echo ""
-echo ""
 echo -n -e "  \e[1;32mMySql installation has finished: ----  \e[0m"
-echo ""
-echo ""
 echo "***************************************************************************"
-echo ""
-echo ""
-echo ""
-echo ""
