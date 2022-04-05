5af26cc049ba368afe2673dff40a9fbd4e5f143c
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2022-04-05 11:21:12 -0400

    Update php7.4.sh
    Composer 2.0 Added

--- php7.4.sh
+++ php7.4.sh
@@ -21,11 +21,11 @@ apt-get install git -y
 apt-get install curl -y
 apt-get install unzip -y
 
-
-curl -s https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
+HASH=`curl -sS https://composer.github.io/installer.sig`
+php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
 composer
-# apt install composer -y
 
 
 # for php 7.4
