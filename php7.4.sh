8d63c9f5e314d381662500067998b7ad82930894
Md Sajedul Alam <47537094+root-bash@users.noreply.github.com>
2022-04-10 01:01:52 -0400

    Update php7.4.sh

--- php7.4.sh
+++ php7.4.sh
@@ -69,9 +69,10 @@ apt install php7.4-xmlrpc -y
 apt install php7.4-xsl -y
 apt install php7.4-zip -y
 
-
-cd /etc/php/7.4/cli/
-# cp php.ini php.ini.bak
+path=$(php -i | grep /.+/php.ini -oE)
+phpconfile="$(dirname "${path}")"
+u_path="${phpconfile}"
+cd u_path
 
 sed -i 's,^file_uploads =.*$,file_uploads = On,' php.ini
 sed -i 's,^allow_url_fopen =.*$,allow_url_fopen = On,' php.ini
